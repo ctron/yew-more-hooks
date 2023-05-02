8ec1e9368d7cef91408c2b7733acfd0f3f112165
Jens Reimann <ctron@dentrassi.de>
2023-05-02 10:52:02 +0200

    docs: add documentation for use_open

--- src/hooks/open.rs
+++ src/hooks/open.rs
@@ -1,6 +1,9 @@
 use yew::prelude::*;
 
-/// Open a target
+/// Open a URL in the provided target.
+///
+/// This provides a callback, which opens the `url` in the provided `target`, using
+/// [`web_sys::Window::open_with_url_and_target`].
 #[hook]
 pub fn use_open<IN>(url: impl Into<String>, target: impl Into<String>) -> Callback<IN, ()>
 where
